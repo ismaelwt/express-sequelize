3826ac8ff3e3cab0aa54fd44af03112fba1e4331
ismael <ismaelnos5@gmail.com>
2017-09-21 13:11:19 -0300

    ajuste usuario

--- server/controllers/empresa.js
+++ server/controllers/empresa.js
@@ -4,14 +4,14 @@ Modulo = require('../models/').Modulo;
 Usuario = require('../models/').Usuario;
 Programa = require('../models/').Programa;
 
-module.exports= {
+module.exports = {
   index(req, res) {
     Empresa.findAll()
       .then(function (Empresas) {
-        
+
         res.status(200).json(Empresas);
-        
-        
+
+
       })
       .catch(function (error) {
         res.status(500).json(error);
@@ -20,44 +20,41 @@ module.exports= {
 
   show(req, res) {
     Empresa.findById(req.params.id)
-    .then(function (Empresa) {
-      res.status(200).json(Empresa);
-    })
-    .catch(function (error){
-      res.status(500).json(error);
-    });
-  },
-
-  create(req, res) {
-    Empresa.create(req.body)
-      .then(function (newEmpresa) {
-
-
-        req.body.usuario.EmpresaId = newEmpresa.id;
-
-        Usuario.create(req.body.usuario).then(function(newUser){
-          if (newUser) {
-            res.status(200).json(newUser);
-          }
-        })
+      .then(function (Empresa) {
+        res.status(200).json(Empresa);
       })
-      .catch(function (error){
+      .catch(function (error) {
         res.status(500).json(error);
       });
   },
 
-  update(req, res) {
-    Empresa.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    })
-    .then(function (updatedRecords) {
-      res.status(200).json(updatedRecords);
-    })
-    .catch(function (error){
-      res.status(500).json(error);
-    });
+  create(req, res) {
+    if (req.query.id) {
+      Empresa.update(req.body, {
+        where: {
+          id: req.query.id
+        }
+      })
+        .then(function (updatedRecords) {
+          res.status(200).json(updatedRecords);
+        })
+        .catch(function (error) {
+          res.status(500).json(error);
+        });
+    } else {
+      Empresa.create(req.body)
+        .then(function (newEmpresa) {
+          req.body.usuario.EmpresaId = newEmpresa.id;
+          Usuario.create(req.body.usuario).then(function (newUser) {
+            if (newUser) {
+              res.status(200).json(newUser);
+            }
+          })
+        })
+        .catch(function (error) {
+          res.status(500).json(error);
+        });
+    }
   },
 
   delete(req, res) {
@@ -66,11 +63,11 @@ module.exports= {
         id: req.params.id
       }
     })
-    .then(function (deletedRecords) {
-      res.status(200).json(deletedRecords);
-    })
-    .catch(function (error){
-      res.status(500).json(error);
-    });
+      .then(function (deletedRecords) {
+        res.status(200).json(deletedRecords);
+      })
+      .catch(function (error) {
+        res.status(500).json(error);
+      });
   }
 };
--- server/controllers/usuario.js
+++ server/controllers/usuario.js
@@ -2,13 +2,13 @@ Usuario = require('../models/').Usuario;
 Empresa = require('../models/').Empresa;
 
 
-module.exports= {
+module.exports = {
   index(req, res) {
-    Usuario.findAll({ include : Empresa})
+    Usuario.findAll({ include: Empresa })
       .then(function (usuarios) {
         res.status(200).json(usuarios);
-        
-        
+
+
       })
       .catch(function (error) {
         res.status(500).json(error);
@@ -16,50 +16,49 @@ module.exports= {
   },
 
   show(req, res) {
-    Usuario.findById(req.params.id, { include : Empresa })
-    .then(function (usuario) {
-      res.status(200).json(usuario);
-    })
-    .catch(function (error){
-      res.status(500).json(error);
-    });
-  },
-
-  create(req, res) {
-    Usuario.create(req.body)
-      .then(function (newUsuario) {
-        res.status(200).json(newUsuario);
+    Usuario.findById(req.params.id, { include: Empresa })
+      .then(function (usuario) {
+        res.status(200).json(usuario);
       })
-      .catch(function (error){
+      .catch(function (error) {
         res.status(500).json(error);
       });
   },
 
-  update(req, res) {
-    Usuario.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    })
-    .then(function (updatedRecords) {
-      res.status(200).json(updatedRecords);
-    })
-    .catch(function (error){
-      res.status(500).json(error);
-    });
+  create(req, res) {
+    if (req.query.id) {
+      Usuario.update(req.body, {
+        where: {
+          id: req.query.id
+        }
+      })
+        .then(function (updatedRecords) {
+          res.status(200).json(updatedRecords);
+        })
+        .catch(function (error) {
+          res.status(500).json(error);
+        });
+    } else {
+      Usuario.create(req.body)
+        .then(function (newUsuario) {
+          res.status(200).json(newUsuario);
+        })
+        .catch(function (error) {
+          res.status(500).json(error);
+        });
+    }
   },
-
   delete(req, res) {
     Usuario.destroy({
       where: {
         id: req.params.id
       }
     })
-    .then(function (deletedRecords) {
-      res.status(200).json(deletedRecords);
-    })
-    .catch(function (error){
-      res.status(500).json(error);
-    });
+      .then(function (deletedRecords) {
+        res.status(200).json(deletedRecords);
+      })
+      .catch(function (error) {
+        res.status(500).json(error);
+      });
   }
 };
